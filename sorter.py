0a4a76eed2381f4b84255318561531b2c57ae090
MelDxKviel <mega1hack52@gmail.com>
2021-10-01 00:25:34 +0700

    update os usage

--- sorter.py
+++ sorter.py
@@ -1,12 +1,12 @@
 import time
 import shutil
-from os import path, listdir
+import os
 
 def main():
     while True:
         path = r"/home/igor/Рабочий стол/"
         end_path = "/home/igor/FromDesktop/" 
-        for item in listdir(path):
+        for item in os.listdir(path):
             if item[0] != '.':
                 item_path = f"{path}/{item}"
                 shutil.move(item_path, end_path)
